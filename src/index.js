fe5923d2e064f91f83e583f2dfa16e8051dab2cc
Srimans7 <139859640+Srimans7@users.noreply.github.com>
2023-11-15 14:29:33 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+
+const rootStyle = {
+  background: 'linear-gradient(to bottom, #b3d9ff, #ffffff)', // Light blue gradient background
+  minHeight: '100vh', // Set minimum height to fill the viewport
+};
+
 ReactDOM.render(
   <React.StrictMode>
+  <div style={rootStyle}>
     <App />
+   </div>
   </React.StrictMode>,
   document.getElementById("root")
 );
